0d01e827dbb40754915832e713a4e456536795fc
Bike Chanokchon <mint.colorful@gmail.com>
2021-08-30 23:26:06 +0700

    Connection Database

---
+++ multiple-image-upload-AspNetCore/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using multiple_image_upload_AspNetCore.Models.db;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace multiple_image_upload_AspNetCore.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly MultipleImageUploadContext _db;
+
+        public UserController(MultipleImageUploadContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var user = _db.Users.ToList();
+            return View(user);
+        }
+    }
+}
---
+++ multiple-image-upload-AspNetCore/Models/db/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace multiple_image_upload_AspNetCore.Models.db
+{
+    public partial class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
