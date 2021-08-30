8a47acd1b2784e3608d3ad2fc053b2a37356af99
Bike Chanokchon <mint.colorful@gmail.com>
2021-08-30 23:01:57 +0700

    Add project files.

---
+++ multiple-image-upload-AspNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace multiple_image_upload_AspNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
