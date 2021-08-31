1fde47b40c5a9171c4b345239ab783a40b4f2e2c
Bike Chanokchon <mint.colorful@gmail.com>
2021-08-31 15:14:15 +0700

    add service upload image

--- multiple-image-upload-AspNetCore/Controllers/UserController.cs
+++ multiple-image-upload-AspNetCore/Controllers/UserController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
 using multiple_image_upload_AspNetCore.Models.db;
+using multiple_image_upload_AspNetCore.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -20,6 +23,59 @@ namespace multiple_image_upload_AspNetCore.Controllers
         {
             var user = _db.Users.ToList();
             return View(user);
+        }
+        public IActionResult Upload()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upload(UploadViewModel data)
+        {
+            if (ModelState.IsValid)
+            {
+                if (string.IsNullOrEmpty(data.Name) || data.Image == null) return View(data);
+
+                var user = _db.Users.FirstOrDefault(u => u.Name == data.Name);
+
+                if (user != null)
+                {
+                    List<string> imagesName = new List<string>();
+
+                    foreach(var item in data.Image)
+                    {
+                        var fileName = Path.GetFileName(item.Name);
+                        var fileExt = Path.GetExtension(fileName);
+                        var tmpName = Guid.NewGuid().ToString();
+                        var newFileName = string.Concat(tmpName, fileExt);
+                        var filePath = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(),"wwwroot","images")).Root + $@"\{newFileName}";
+
+                        using (FileStream fs = System.IO.File.Create(filePath))
+                        {
+                            item.CopyTo(fs);
+                            fs.Flush();
+                        }
+                        imagesName.Add(newFileName);
+                    }
+
+                    foreach(var item in imagesName)
+                    {
+                        Image image = new Image
+                        {
+                            UserId = user.Id,
+                            ImageName = item
+                        };
+                        await _db.Images.AddAsync(image);
+                    }
+                    await _db.SaveChangesAsync();
+                    return RedirectToAction("Index","Home");
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            return View(data);
         }
     }
 }
---
+++ multiple-image-upload-AspNetCore/ViewModels/UploadViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace multiple_image_upload_AspNetCore.ViewModels
+{
+    public class UploadViewModel
+    {
+        [Required(ErrorMessage = "Please enter your name.")]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please select your image.")]
+        [Display(Name = "Upload image")]
+        public List<IFormFile> Image { get; set; }
+    }
+}
